dc2d32d76dcdc7ece640b605ea6a72081dd494f6
JackMcAulay <47449092+JackMcAulay@users.noreply.github.com>
2022-02-15 12:44:35 +0000

    Added Converter

---
+++ Converter/Converter/Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace Converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbLbs = new System.Windows.Forms.TextBox();
+            this.tbKg = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnConvertLbsKg = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbFt = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnConvertFtM = new System.Windows.Forms.Button();
+            this.tbM = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tbPt = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btnConvertPtL = new System.Windows.Forms.Button();
+            this.tbL = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(188, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Pound to Kilogram";
+            // 
+            // tbLbs
+            // 
+            this.tbLbs.Location = new System.Drawing.Point(17, 41);
+            this.tbLbs.Name = "tbLbs";
+            this.tbLbs.Size = new System.Drawing.Size(61, 20);
+            this.tbLbs.TabIndex = 1;
+            this.tbLbs.Text = "0";
+            this.tbLbs.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // tbKg
+            // 
+            this.tbKg.Location = new System.Drawing.Point(191, 46);
+            this.tbKg.Name = "tbKg";
+            this.tbKg.Size = new System.Drawing.Size(61, 20);
+            this.tbKg.TabIndex = 2;
+            this.tbKg.Text = "0";
+            this.tbKg.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(78, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "lbs";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(252, 53);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(19, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "kg";
+            // 
+            // btnConvertLbsKg
+            // 
+            this.btnConvertLbsKg.Location = new System.Drawing.Point(104, 43);
+            this.btnConvertLbsKg.Name = "btnConvertLbsKg";
+            this.btnConvertLbsKg.Size = new System.Drawing.Size(75, 23);
+            this.btnConvertLbsKg.TabIndex = 5;
+            this.btnConvertLbsKg.Text = "Convert";
+            this.btnConvertLbsKg.UseVisualStyleBackColor = true;
+            this.btnConvertLbsKg.Click += new System.EventHandler(this.btnConvertLbsKg_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(13, 87);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(140, 25);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Feet to Meter";
+            // 
+            // tbFt
+            // 
+            this.tbFt.Location = new System.Drawing.Point(18, 115);
+            this.tbFt.Name = "tbFt";
+            this.tbFt.Size = new System.Drawing.Size(61, 20);
+            this.tbFt.TabIndex = 7;
+            this.tbFt.Text = "0";
+            this.tbFt.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(79, 122);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(13, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "ft";
+            // 
+            // btnConvertFtM
+            // 
+            this.btnConvertFtM.Location = new System.Drawing.Point(104, 113);
+            this.btnConvertFtM.Name = "btnConvertFtM";
+            this.btnConvertFtM.Size = new System.Drawing.Size(75, 23);
+            this.btnConvertFtM.TabIndex = 9;
+            this.btnConvertFtM.Text = "Convert";
+            this.btnConvertFtM.UseVisualStyleBackColor = true;
+            this.btnConvertFtM.Click += new System.EventHandler(this.btnConvertFtM_Click);
+            // 
+            // tbM
+            // 
+            this.tbM.Location = new System.Drawing.Point(191, 115);
+            this.tbM.Name = "tbM";
+            this.tbM.Size = new System.Drawing.Size(61, 20);
+            this.tbM.TabIndex = 10;
+            this.tbM.Text = "0";
+            this.tbM.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(252, 122);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(15, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "m";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(13, 161);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(121, 25);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Pint to Litre";
+            // 
+            // tbPt
+            // 
+            this.tbPt.Location = new System.Drawing.Point(17, 189);
+            this.tbPt.Name = "tbPt";
+            this.tbPt.Size = new System.Drawing.Size(62, 20);
+            this.tbPt.TabIndex = 13;
+            this.tbPt.Text = "0";
+            this.tbPt.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(79, 196);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(16, 13);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "pt";
+            // 
+            // btnConvertPtL
+            // 
+            this.btnConvertPtL.Location = new System.Drawing.Point(104, 188);
+            this.btnConvertPtL.Name = "btnConvertPtL";
+            this.btnConvertPtL.Size = new System.Drawing.Size(75, 23);
+            this.btnConvertPtL.TabIndex = 15;
+            this.btnConvertPtL.Text = "Convert";
+            this.btnConvertPtL.UseVisualStyleBackColor = true;
+            this.btnConvertPtL.Click += new System.EventHandler(this.btnConvertPtL_Click);
+            // 
+            // tbL
+            // 
+            this.tbL.Location = new System.Drawing.Point(189, 191);
+            this.tbL.Name = "tbL";
+            this.tbL.Size = new System.Drawing.Size(63, 20);
+            this.tbL.TabIndex = 16;
+            this.tbL.Text = "0";
+            this.tbL.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(252, 198);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(9, 13);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "l";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(290, 232);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.tbL);
+            this.Controls.Add(this.btnConvertPtL);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.tbPt);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.tbM);
+            this.Controls.Add(this.btnConvertFtM);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tbFt);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnConvertLbsKg);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbKg);
+            this.Controls.Add(this.tbLbs);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbLbs;
+        private System.Windows.Forms.TextBox tbKg;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnConvertLbsKg;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbFt;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnConvertFtM;
+        private System.Windows.Forms.TextBox tbM;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox tbPt;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button btnConvertPtL;
+        private System.Windows.Forms.TextBox tbL;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ Converter/Converter/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Converter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConvertLbsKg_Click(object sender, EventArgs e)
+        {
+            double lbsValue = double.Parse(tbLbs.Text);
+            double kgValue = lbsValue * 0.453592;
+            tbKg.Text = kgValue.ToString("0.00");
+        }
+
+        private void btnConvertFtM_Click(object sender, EventArgs e)
+        {
+            double ftValue = double.Parse(tbFt.Text);
+            double mValue = ftValue * 0.3048;
+            tbM.Text = mValue.ToString("0.00");
+        }
+
+        private void btnConvertPtL_Click(object sender, EventArgs e)
+        {
+            double ptValue = double.Parse(tbPt.Text);
+            double lValue = ptValue * 0.568261;
+            tbL.Text = lValue.ToString("0.00");
+        }
+    }
+}
